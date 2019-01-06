4747224f7b37c243412f0ec9aa46ce333e25cb30
Max <max@192.168.1.40>
2019-01-06 09:12:14 +0700

    initial react project and package

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Button } from "./components/Logo";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button>sss</Button>
+        test
+      </div>
+    );
+  }
+}
+
+export default App;
